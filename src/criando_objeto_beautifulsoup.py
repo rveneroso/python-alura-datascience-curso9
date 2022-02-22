4cf8ae2eb58b55043d1340df87406e99af0e9351
Renato Veneroso <rveneroso@gmail.com>
2022-02-21 22:10:13 -0300

    Criação do script que cria e manipula objetos do tipo BeautifulSoup.

---
+++ src/criando_objeto_beautifulsoup.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import util
+
+url = 'https://alura-site-scraping.herokuapp.com/index.php'
+html = util.obtem_html(url)
+soup = BeautifulSoup(html, "html.parser") # Objeto do tipo <class 'bs4.BeautifulSoup'>
+
+# Acessando a tag html de um documento
+print(soup.html)
+
+# Acessando a tag head de um documento
+print(soup.head)
+
+# Acessando a tag title de um documento
+print(soup.head.title)
+# Outra forma é acessar diretamente a tag title mas isso só vale quando só há uma tag do tipo no documento.
+print(soup.title)
+
+# Navegando pela árvore de tags div até chegar na tag h5.
+print(soup.body.div.div.div.div.h5)
+
+# Pegando apenas o valor da tag
+print(soup.body.div.div.div.div.h5.get_text())
+# Ou ainda
+print(soup.body.h5.getText())
+# Acessando a tag body de um documento
+print(soup.body)
+# Obtendo somente o texto (ignorando as tags) do documento html completo
+print(soup.get_text())
